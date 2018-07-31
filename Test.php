4aaa41a34135cd81970106448b8e87d802964250
fmartuica <41463838+fmartuica@users.noreply.github.com>
2018-07-31 11:13:44 +0300

    Update Test.php

--- Test.php
+++ Test.php
@@ -16,26 +16,9 @@
   padding: 0px 38px;
 }
 </style>
-<link rel="stylesheet" href="quizz.css">
+
 <body>
 
-<div id="div1" class="fa"></div>
-
-<script>
-function hourglass() {
-  var a;
-  a = document.getElementById("div1");
-  a.innerHTML = "&#xf251;";
-  setTimeout(function () {
-      a.innerHTML = "&#xf252;";
-    }, 1000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf253;";
-    }, 2000);
-}
-hourglass();
-setInterval(hourglass, 3000);
-</script>
 
 <h1><time>00:00:00</time></h1>
 <script>
